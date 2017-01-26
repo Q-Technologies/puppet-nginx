6e4482f978e96c95b20083cdcb5d5dc099a21ee5
Matthew Mallard <matt@Q-Technologies.com.au>
2017-01-26 15:33:46 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# puppet-nginx
+Puppet module to manage Nginx mostly through templates.
+
+## Instructions
+T.B.A.
+
+## Issues
+This module is using hiera data that is embedded in the module rather than using a params class.  This may not play nicely with other modules using the same technique unless you are using hiera 3.0.6 and above (PE 2015.3.2+).
---
+++ manifests/init.pp
@@ -0,0 +1,43 @@
+class nginx (
+  # Class parameters are populated from module hiera data
+  Data $domains = {},
+){
+
+  $nginx_conf = "/etc/nginx"
+  $nginx_vhosts_conf = "${nginx_conf}/vhosts.d"
+
+  file { "${nginx_conf}":
+    ensure  => directory,
+  }
+
+  file { "${nginx_vhosts_conf}":
+    ensure  => directory,
+    recurse => true,
+    purge   => true,
+  }
+
+  # NGINX Virtual Host definition
+  $domains.each | $domain, $config | {
+    file { "${nginx_vhosts_conf}/${domain}.conf":
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0640',
+      content => epp('nginx/vhost_conf.epp', { domain => $domain, config => $config } ),
+      notify  => Service['nginx'],
+    }
+  }
+  file { "${nginx_conf}/nginx.conf":
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
+    content => epp('nginx/nginx_conf.epp', { } ),
+    notify  => Service['nginx'],
+  }
+
+  service { "nginx":
+    ensure  => true,
+    enable  => true,
+  }
+}
